dcb4cdc3d1d131af03553a103e378d094fe02c66
Alexander J. Busch <alexander.busch.ctr@us.af.mil>
2026-03-27 15:03:26 +0000

    live grep kemap

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -31,7 +31,7 @@ return {
             vim.keymap.set("n", "<C-p>", find_files)
 
             vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = 'Telescope find files' })
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = 'Telescope live grep' })
+            vim.keymap.set("n", "<leader>fz", builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = 'Telescope buffers' })
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = 'Telescope help tags' })
 
