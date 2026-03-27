14b97ffc1fac801df6b0c99078031e785c78cb50
Alex Busch <abusch8@huskers.unl.edu>
2026-03-27 11:24:12 -0500

    mini.splitjoin

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "echasnovski/mini.pairs",
+        opts = {},
+    },
+    {
+        "nvim-mini/mini.surround",
+        opts = {},
+    },
+    {
+        "nvim-mini/mini.splitjoin",
+        opts = {},
+    }
+}
