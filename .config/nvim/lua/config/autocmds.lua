d9621f1362eaf6e0b0beb70e1843ea46683f4a80
Alexander J. Busch <alexander.busch.ctr@us.af.mil>
2026-04-17 17:01:02 +0000

    Removed autocmd

--- .config/nvim/lua/config/autocmds.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.py",
-    callback = function()
-        vim.cmd("silent noautocmd !ruff check % --select I --fix")
-        vim.cmd("edit!")
-    end
-})
-
