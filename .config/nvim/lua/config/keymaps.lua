0709fe40ef689edcf85f9753e079cca95360a052
Alexander J. Busch <alexander.busch.ctr@us.af.mil>
2026-04-17 14:44:38 +0000

    Added goto keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 vim.keymap.set("n", "<leader>e", function()
     vim.diagnostic.open_float(nil, { scope = "line" })
 end)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local buffer = args.buf
+        local opts = { noremap = true, silent = true, buffer = buffer }
+
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    end,
+})
+
